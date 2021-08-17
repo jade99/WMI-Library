d4c5f9bd581b2648e3698d0f37a2dca6ed472118
Philipp Wurzer <philippwurzer@outlook.de>
2021-08-17 17:21:08 +0200

    created WmiObject base class

---
+++ WMI Library/src/WmiObject.cpp
@@ -0,0 +1,81 @@
+#include "WmiObject.h"
+
+#include <iostream>
+#include <atlbase.h>
+
+namespace Jade
+{
+	WmiObject::WmiObject(const std::wstring& arg_strObjectName)
+	{
+		const auto length = arg_strObjectName.size();
+		_strObjectName = new wchar_t[length + 1];
+		wcscpy_s(_strObjectName, length, arg_strObjectName.c_str());
+
+		std::cout << "[WmiObject] created!" << std::endl;
+	}
+
+	WmiObject::WmiObject(const WmiObject& arg_wmiOther)
+	{
+		_pObject = arg_wmiOther._pObject;
+
+		const auto lenName = wcslen(arg_wmiOther._strObjectName);
+		_strObjectName = new wchar_t[lenName + 1];
+		wcscpy_s(_strObjectName, lenName, arg_wmiOther._strObjectName);
+
+		const auto lenPath = wcslen(arg_wmiOther._strObjectPath);
+		_strObjectPath = new wchar_t[lenPath + 1];
+		wcscpy_s(_strObjectPath, lenPath, arg_wmiOther._strObjectPath);
+
+		std::cout << "[WmiObject] copied!" << std::endl;
+	}
+
+	WmiObject::WmiObject(WmiObject&& arg_wmiOther) noexcept
+	{
+		_pObject = arg_wmiOther._pObject;
+		arg_wmiOther._pObject = nullptr;
+
+		_strObjectName = arg_wmiOther._strObjectName;
+		arg_wmiOther._strObjectName = nullptr;
+
+		_strObjectPath = arg_wmiOther._strObjectPath;
+		arg_wmiOther._strObjectPath = nullptr;
+
+		std::cout << "[WmiObject] moved!" << std::endl;
+	}
+
+	WmiObject::~WmiObject()
+	{
+		delete _strObjectName;
+		delete _strObjectPath;
+
+		std::cout << "[WmiObject] destroyed!" << std::endl;
+	}
+
+	void WmiObject::RetrievePath()
+	{
+		if (_pObject == nullptr) return;
+
+		CComVariant vtPATH;
+		const auto hres = _pObject->Get(L"__PATH", 0, &vtPATH, nullptr, nullptr);
+
+		if (FAILED(hres) || vtPATH.vt != VT_BSTR) return;
+
+		const auto length = wcslen(vtPATH.bstrVal);
+		wcscpy_s(_strObjectPath, length, vtPATH.bstrVal);
+	}
+
+	IWbemClassObject* WmiObject::GetObjectPtr() const
+	{
+		return _pObject;
+	}
+
+	std::wstring WmiObject::GetObjectName() const
+	{
+		return std::wstring(_strObjectName);
+	}
+
+	std::wstring WmiObject::GetObjectPath() const
+	{
+		return std::wstring(_strObjectPath);
+	}
+}
