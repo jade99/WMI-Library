560545c8ee8ffe8ebad5beae2edbf9e0ee99d2d7
jade99 <philippwurzer@outlook.de>
2021-08-18 09:22:53 +0200

    fixed object initialization refactoring

--- WMI Library/src/Wmi.h
+++ WMI Library/src/Wmi.h
@@ -21,34 +21,35 @@ namespace Jade
 		IWbemLocator* _pLocator = nullptr;
 		IWbemServices* _pServices = nullptr;
 
-		static SWmi _s_WmiInstance;
-
 	public:
 		SWmi(const SWmi& wmi) = delete;
 		
 		static IEnumWbemClassObject* Query(const std::wstring& arg_strQuery) 
-		{ return _s_WmiInstance.QueryImpl(arg_strQuery); };
+		{ return Get().QueryImpl(arg_strQuery); };
 
-		static WmiClass GetWmiClass(const std::wstring& arg_strClassName)
-		{ return _s_WmiInstance.GetWmiClass(arg_strClassName); };
+		static WmiClass GetClass(const std::wstring& arg_strClassName)
+		{ return Get().GetClassImpl(arg_strClassName); };
 
 
 		static void ExecMethod(const WmiInstance<WmiMethod>& arg_wmiMethodInstance, const std::wstring& arg_strAltObjectPath)
-		{ _s_WmiInstance.ExecMethodImpl(arg_wmiMethodInstance, arg_strAltObjectPath); };
+		{ Get().ExecMethodImpl(arg_wmiMethodInstance, arg_strAltObjectPath); };
 
 		static void ExecMethod(const WmiInstance<WmiMethod>& arg_wmiMethodInstance)
-		{ _s_WmiInstance.ExecMethodImpl(arg_wmiMethodInstance, arg_wmiMethodInstance.GetObjectPath()); };
+		{ Get().ExecMethodImpl(arg_wmiMethodInstance, arg_wmiMethodInstance.GetObjectPath()); };
 
 
 		static bool Connect(const std::wstring& arg_strWmiPath, const wchar_t* const arg_strUsername, const wchar_t* const arg_strPassword)
-		{  return _s_WmiInstance.ConnectImpl(arg_strWmiPath, arg_strUsername, arg_strPassword); };
+		{  return Get().ConnectImpl(arg_strWmiPath, arg_strUsername, arg_strPassword); };
 
 		static void Disconnect()
-		{ _s_WmiInstance.DisconnectImpl(); };
+		{ Get().DisconnectImpl(); };
 
 	private:
 		SWmi();
 		~SWmi();
+
+		static SWmi& Get()
+		{ static SWmi instance; return instance; }
 
 		IEnumWbemClassObject* QueryImpl(const std::wstring& arg_strQuery) const;
 		WmiClass GetClassImpl(const std::wstring& arg_strClassName) const;
